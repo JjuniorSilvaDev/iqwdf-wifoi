da7aff0ca7dbb3aca7e56e1adbea279b92fb4efe
JjuniorSilvaDev <juniorsilvanp@gmail.com>
2024-12-07 16:55:55 -0300

    Add the card with the ride history

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.teste.appdetaxi"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.teste.appdetaxi"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
